29e77b0df2d69cf2093941aaa7ec0c4c443157d0
MammadliHuseyn <54790582+MammadliHuseyn@users.noreply.github.com>
2021-10-18 23:59:46 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+// let name = "Iftixar";
+// let age = 21;
+// let isMarried = false;
+// let variable; // undefined
+
+// console.log(typeof(name));
+// console.log(typeof(age));
+// console.log(typeof(isMarried));
+// console.log(typeof(variable));
+
+// console.log(num1 - num2);
+// console.log(num1 / num2);
+// console.log(num1 * num2);
+// console.log(num1 + parseInt(num2));
+// console.log(+num1 + Number(num2));
+// console.log(num1 ** num2);
+// num1 *= 2;
+// console.log(num1 % num2);
+
+// let num1 = 12;
+// let num2 = 5;
+// let num3 = 10;
+
+// if (num1 > num2 || num2 > num3 || num1 > num3) {
+//   // console.log(num1 + " boyukdur " + num2)
+//   // console.log(`${num1} boyukdur ${num2}`);
+// } else if (num1 == num2) {
+//   console.log(`${num1} = ${num2}`);
+// } else {
+//   console.log(`${num1} kicikdir ${num2}`);
+// }
+
+
+// let season = prompt("fesil adi daxil edin");
+// season = season.toLowerCase();
+
+// switch (season) {
+//     case "yaz":
+//         console.log("Havalar istilesir");
+//         break;
+//     case "yay":
+//         console.log("Bishdik");
+//         break;
+//     case "payiz":
+//         console.log("Yagish yagir");
+//         break;
+//     case "qish":
+//         console.log("Donduq");
+//         break;
+//     default:
+//         console.log("Duzgun fesil daxil edin");
+//         break;
+// }
+
+
+// let num1 = "4";
+// let num2 = 4;
+
+// console.log(num1 === num2);
+
+// alert("salam");
+
+// let age = prompt("what is your age");
+
+// console.log(+age + 2);
+
+// let isTrue =  confirm("Are you sure?");
+
+// console.log(isTrue);
+
